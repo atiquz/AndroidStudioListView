d71a1bf8ad97f509dd5846516c95171d8541c91c
Md Atiquz Zaman <atiquzz42@gmail.com>
2024-02-05 12:43:10 +0530

    First Commit

---
+++ app/src/main/java/com/atidevelopers/androidstudiolistview/MainActivity.java
@@ -0,0 +1,33 @@
+package com.atidevelopers.androidstudiolistview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    String[] cities = {"Mumbai", "Chennai", "Bangalore", "Guwahati", "Allahabad"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.list);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, cities);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "Clicked " + cities[position], Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
